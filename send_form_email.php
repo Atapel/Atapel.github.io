c8329579122bf4352431e406b92680f86f67ba47
Andrew <andrew@Andrews-MBP.fios-router.home>
2019-05-15 22:19:36 -0400

    1

--- send_form_email.php
+++ send_form_email.php
@@ -2,24 +2,17 @@
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $roobt = $_POST['roobt'];
     $from = 'From: andrew@andrewbregman.com'; 
     $to = 'djbreg@gmail.com'; 
     $subject = 'Hello';
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
     if ($_POST['submit']) {
-      if($roobt == "robot")
-        if (mail ($to, $subject, $body, $from)) { 
+      if (mail ($to, $subject, $body, $from)) { 
         echo '<p>Your message has been sent!</p>';
-        else{
-          echo '<p>Uh oh, something went wrong!</p>';
-        }
-      }
-      else{
-          echo '<p>Uh oh, something went wrong!</p>';
-      } 
+            print phpinfo();  
 
+      } 
       else { 
           echo '<p>Something went wrong, go back and try again!</p>'; 
       }
